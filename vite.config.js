f3f739ce6e68d443b01786a8223a649e12f21795
islam-foda <Eslam.foda@hotmail.com>
2024-11-25 00:30:26 +0200

    fix merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "Fylo-Landing_page",
-  plugins: [react()],
-});
-=======
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -16,4 +6,3 @@ export default defineConfig({
 	base: 'fylo-landing-page-with-two-column-layout-master/',
   plugins: [react()],
 })
->>>>>>> 25cb2b7e0d7eca6cdd3df081c871b65fb9177328
