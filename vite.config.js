df0458b754ade0a39425c980f6814e988b33d7cb
islam-foda <Eslam.foda@hotmail.com>
2024-11-25 00:44:24 +0200

    check for vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: 'fylo-landing-page-with-two-column-layout-master/',
   plugins: [react()],
-})
+  base: "/Fylo-Landing_page/",
+});
